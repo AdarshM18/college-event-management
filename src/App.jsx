fcf9a2ac4f01a42f1fb5a737c15a32082b1377d1
Adarsh Mishra <mt2402101004@iiti.ac.in>
2025-03-29 22:18:21 +0530

    Updated code with new features

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { HashRouter as Router, Routes, Route, Link } from "react-router-dom";
 import { onAuthStateChanged, signInWithPopup, GoogleAuthProvider, signOut } from "firebase/auth";
 import { auth } from "./firebase-config";
 import Dashboard from "./components/Dashboard";
